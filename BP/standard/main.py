8393b69a0dd14b47832ea96ab92090793dbceec8
brilli4ntc0der <cby141994@gmail.com>
2020-10-08 12:13:46 +0800

    routine update

--- BP/standard/main.py
+++ BP/standard/main.py
@@ -1,4 +1,64 @@
 from sklearn import datasets
+import numpy as np
+from random import SystemRandom
+
+
+def sigmod(x):
+    return 1.0 / (np.exp(-x) + 1.0)
+
+
+def squared_error(x, y):
+    assert len(x) == len(y)
+    n = len(x)
+    err = 0.0
+    for i in range(n):
+        err += (x[i] - y[i]) ** 2
+    return 0.5 * err
+
+
+class Net(object):
+    def __init__(self, d, q, ll, eta):
+        rand = SystemRandom()
+        self.d = d
+        self.q = q
+        self.ll = ll
+        self.eta = eta
+
+        self.v = [[[rand()] for i in range(d)] for j in range(q)]
+        self.gamma = [[rand()] * q]
+        self.w = [[[rand()] for i in range(q)] for j in range(ll)]
+        self.theta = [[rand()] * ll]
+
+    def loss(self, x, y):
+        assert isinstance(x, list) == True
+        assert len(x) == self.d
+        alpha = [[0.0] for i in range(self.q)]
+        alpha = np.array(alpha)
+        for i in range(self.d):
+            for h in range(self.q):
+                alpha[h] += self.v[i][h] * x[i]
+        b = [[0.0] for i in range(self.q)]
+        b = np.array(b)
+        for h in range(self.q):
+            b[h] += sigmod(alpha[h] - self.gamma[h])
+
+        beta = [[0.0] for i in range(self.ll)]
+        for j in range(self.ll):
+            for h in range(self.q):
+                beta[j] += self.w[h][j] * b[h]
+
+        y_ = [[0.0] for i in range(self.ll)]
+        y_ = np.array(y_)
+        for j in range(self.ll):
+            y_ = sigmod(beta[j] - self.theta[j])
+
+        g = [[0.0] for i in range(self.ll)]
+        for j in range(self.ll):
+            g[j] = y_[j] * (y_[j] - y[j]) * (1.0 - y_[j])
+        e = [[0.0] for i in range()]
+
+        return squared_error(y, y_), y_, g,
+
 
 if __name__ == '__main__':
     iris = datasets.load_iris()
@@ -10,5 +70,21 @@ if __name__ == '__main__':
     print('Iris data size :', irisFeatures.shape)
     print('Iris label size :', irisLabels.shape)
 
+    train_data = []
+    train_label = []
+    test_data = []
+    test_label = []
 
+    for i in range(3):
+        for j in range(40):
+            train_data.append(irisFeatures[i * 50 + j])
+            train_label.append(irisLabels[i * 50 + j])
+        for j in range(10):
+            test_data.append(irisFeatures[i * 50 + 40 + j])
+            test_label.append(irisLabels[i * 50 + 40 + j])
+
+    train_data = np.array(train_data)
+    train_label = np.array(train_label)
+    test_data = np.array(test_data)
+    test_label = np.array(test_label)
 
