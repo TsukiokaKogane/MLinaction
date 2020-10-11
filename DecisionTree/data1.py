530b1795e458b52f649b1921bee2fb80b04b6b99
brilli4ntc0der <cby141994@gmail.com>
2020-10-11 17:02:54 +0800

    decision tree first commit

---
+++ DecisionTree/data1.py
@@ -0,0 +1,25 @@
+rawdata = [
+    [1, '<30', 'High', 'No', 'Fair', 'No'],
+    [1, '<30', 'High', 'No', 'Excellent', 'No'],
+    [1, '30-40', 'High', 'No', 'Fair', 'Yes'],
+    [1, '>40', 'Medium', 'No', 'Fair', 'Yes'],
+    [1, '>40', 'Low', 'Yes', 'Fair', 'Yes'],
+    [1, '>40', 'Low', 'Yes', 'Fair', 'Yes'],
+    [1, '>40', 'Low', 'Yes', 'Excellent', 'No'],
+    [1, '30-40', 'Low', 'Yes', 'Excellent', 'Yes'],
+    [1, '<30', 'Medium', 'No', 'Fair', 'No'],
+    [1, '<30', 'Low', 'Yes', 'Fair', 'Yes'],
+    [1, '>40', 'Medium', 'Yes', 'Fair', 'Yes'],
+    [1, '<30', 'Medium', 'Yes', 'Excellent', 'Yes'],
+    [1, '30-40', 'Medium', 'No', 'Excellent', 'Yes'],
+    [1, '30-40', 'High', 'Yes', 'Fair', 'Yes'],
+    [1, '>40', 'Medium', 'No', 'Excellent', 'No'],
+]
+
+domain = [
+    ['<30', '30-40', '>40'],
+    ['High', 'Medium', 'Low'],
+    ['Yes', 'No'],
+    ['Fair', 'Excellent'],
+    ['Yes', 'No']
+]
---
+++ DecisionTree/main.py
@@ -0,0 +1,5 @@
+from data1 import rawdata, domain
+
+if __name__ == '__main__':
+    print(rawdata)
+    print(domain)
